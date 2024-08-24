15fa228795faa634d712fca9ee20c90e645ea993
Güney Kılıçel <kilicelguney@gmail.com>
2024-08-24 21:23:59 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import { motion, AnimatePresence, LayoutGroup } from "framer-motion";
+import "./sass/main.scss";
+
+// Components
+import Header from "./components/Header";
+import Banner from "./components/Banner";
+import Loader from "./components/Loader";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    loading
+      ? document.querySelector("body").classList.add("loading")
+      : document.querySelector("body").classList.remove("loading");
+  }, [loading]);
+
+  return (
+    <LayoutGroup>
+      <AnimatePresence>
+        {loading ? (
+          <motion.div key='loader'>
+            <Loader setLoading={setLoading} />
+          </motion.div>
+        ) : (
+          <>
+            <Header />
+            <Banner />
+            {!loading && (
+              <div className='transition-image final'>
+                <motion.img
+                  transition={{ ease: [0.6, 0.01, 0.2, 1], duration: 1.6 }} // Geçerli cubic-bezier değerini kullanın
+                  src={process.env.PUBLIC_URL + `/images/image-2.jpg`}
+                  layoutId='main-image-1'
+                />
+              </div>
+            )}
+          </>
+        )}
+      </AnimatePresence>
+    </LayoutGroup>
+  );
+}
+
+export default App;
